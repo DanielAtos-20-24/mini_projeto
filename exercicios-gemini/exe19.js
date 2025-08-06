6239726c5326392c302dc26e5f22df78b1bd5f6a
Daniel Moreira <danielmoreicustodio1999@gmail.com>
2025-08-06 15:55:18 -0300

    Remoção de comentarios.

--- exercicios-gemini/exe19.js
+++ exercicios-gemini/exe19.js
@@ -5,7 +5,7 @@ for(let i = 0; i < 4; i++) {
     let numeroArray;
 
     do{
-        numeroArray = parseInt(Math.random() * 10 + 1) //8
+        numeroArray = parseInt(Math.random() * 10 + 1) 
     } while (arrayVazia.includes(numeroArray));
 
     arrayVazia.push(numeroArray)
